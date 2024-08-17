a141c03faf1205376fa4ab551ba7e920d4a55269
campossergio <sermarcia@hotmail.com>
2024-08-17 17:31:53 -0300

    Criação das Classes Contador e ParametrosInvalidosException com controle de execeção

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.text.ParseException;
+
+public class Contador {
+    public static void main(String[] args) {
+        System.out.println("Desafio Contorle de Fluxo");
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        System.out.println("O parâmetro Um é " + parametroUm + " e o parâmetro Dois é " + parametroDois);
+
+        //contar(parametroUm, parametroDois);
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int contador = 0; contador < contagem; contador++) {
+            System.out.println("Imprimindo o número " + (contador + 1));
+        }
+    }
+}
